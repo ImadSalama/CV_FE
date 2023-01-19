8fa5ada510af53d2becb00f2e8f1e3ccd81355a6
ShareefAlawneh <shareefalawneh2014@gmail.com>
2023-01-19 23:36:33 +0200

    contactForm

--- src/components/Utils/ContactForm/ContactForm.js
+++ src/components/Utils/ContactForm/ContactForm.js
@@ -30,25 +30,60 @@ function ContactForm(props) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [messages, setMessages] = useState("");
+  // errors
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [messagesError, setMessagesError] = useState("");
 
   const userContact = useSelector((state) => state.userContact);
   const { userContactInfo, loading, error } = userContact;
 
-  // const dispatch = useDispatch();
-  // const submitHandler = (e) => {
-  //   e.preventDefault();
-  //   dispatch(contact(email, name, messages));
+  const validateEmail = () => {
+    if (!email) {
+      setEmailError("Email is Required");
+    }
 
-  //   messageApi.open({
-  //     type: "success",
-  //     content: "The message has been sent successfully",
-  //   });
-  //   // window.location.reload(true);
-  // };
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      setEmailError("Enter a Valid Email");
+    }
+
+    return (
+      !!email &&
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    );
+  };
+
+  const validateName = () => {
+    if (!name) {
+      setNameError("Name is Required");
+    }
+    return !!name;
+  };
+
+  const validateMessage = () => {
+    if (!messages) {
+      setMessagesError("Message is Required");
+    }
+
+    return !!messages;
+  };
 
   const dispatch = useDispatch();
   const submitHandler = (e) => {
     e.preventDefault();
+    const isValidName = validateName();
+    const isValidEmail = validateEmail();
+    const isValidMessage = validateMessage();
+
+    if (!isValidEmail || !isValidMessage || !isValidName) {
+      return;
+    }
 
     dispatch(
       contact(email, name, messages, (err = false) => {
@@ -89,37 +124,65 @@ function ContactForm(props) {
       </h1>
       <Form {...layout} style={{ fontFamily: "AvenirTextBlack" }}>
         <Item>
-          <Input
-            type="text"
-            className="px-5"
-            placeholder="Email Address"
-            size="large"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
+          <Form.Item
+            validateStatus={emailError ? "error" : ""}
+            help={emailError}
+            wrapperCol={{
+              xl: 24,
+            }}
+          >
+            <Input
+              type="email"
+              className="px-5 col-xl-12"
+              placeholder="Email Address"
+              size="large"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </Form.Item>
           <div className="px-4 py-2" />
-          <Input
-            type="text"
-            className="px-5"
-            placeholder="Name"
-            size="large"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-          />
+          <Form.Item
+            validateStatus={nameError ? "error" : ""}
+            help={nameError}
+            wrapperCol={{
+              xl: 24,
+            }}
+          >
+            <Input
+              type="text"
+              className="px-5"
+              placeholder="Name"
+              size="large"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+            />
+          </Form.Item>
         </Item>
         {/* <Item>
           <Input type="text" placeholder="Enter Subject Here" size="large" />
         </Item> */}
         <Item>
-          <Input
-            className="mb-2"
-            type="text"
-            placeholder="Let us know how we can help you!"
-            size="large"
-            style={{ height: 120 }}
-            onChange={(e) => setMessages(e.target.value)}
-            value={messages}
-          />
+          <Form.Item
+            validateStatus={messagesError ? "error" : ""}
+            help={messagesError}
+            style={{ width: "100%" }}
+            wrapperCol={{
+              xl: 24,
+            }}
+          >
+            <Input
+              className="mb-2"
+              type="text"
+              placeholder="Let us know how we can help you!"
+              size="large"
+              style={{ height: 120 }}
+              onChange={(e) => setMessages(e.target.value)}
+              value={messages}
+              required
+            />
+          </Form.Item>
         </Item>
       </Form>
       <div className="text-center">
